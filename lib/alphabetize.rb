e639bd537cb88df8372524f1083719632b4bc9b2
Stella McEwen <mcewen.stella@gmail.com>
2019-04-24 22:54:56 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
 alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 arr.sort_by do |phrase| 
-  phrase.split.map do |letters|
+  phrase.split("").map do |letters|
     alphabet.index(letters)
 end
 end
